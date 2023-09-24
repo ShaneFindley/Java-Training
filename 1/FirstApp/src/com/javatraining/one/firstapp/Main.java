e2eb93f316813635a8cc0d084ea0622807f6252d
Shane Findley <shane.findley@sonarsource.com>
2023-09-24 18:00:18 +0200

    Testing Java

---
+++ 1/FirstApp/src/com/javatraining/one/firstapp/Main.java
@@ -0,0 +1,9 @@
+package com.javatraining.one.firstapp;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("First line from app");
+        System.out.println("Second line from app");
+        System.out.println("Third line from app");
+    }
+}
